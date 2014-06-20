170c27654834fe7f37926db08ed7126840493080
pdebelak <pdebelak@gmail.com>
2014-06-20 14:12:45 -0500

    Style improvements. Also separated style, script, and html files

---
+++ application.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+  var dataset = [];
+  $("#addFurniture").hide();
+  $('#roomSize').on('submit', function(e) {
+    e.preventDefault();
+    $('#roomSize').hide();
+    $('#addFurniture').show();
+    var room_width = $('input[name=width]').val();
+    var room_length = $('input[name=length]').val();
+    var a = (room_width < room_length) ? room_width : room_length;
+    function dragmove(d) {
+      d3.select(this).attr("y", function() {
+        if (d3.event.y < 0) {
+          return 0;
+        } else if (d3.event.y > (scale(room_width) - this.getBBox().height)) {
+          return scale(room_width) - this.getBBox().height;
+        } else {
+          return d3.event.y;
+        }
+      })
+      .attr("x", function() {
+        if (d3.event.x < 0) {
+          return 0;
+        } else if (d3.event.x > (scale(room_length) - this.getBBox().width)) {
+          return scale(room_length) - this.getBBox().width;
+        } else {
+          return d3.event.x;
+        }
+      });
+    }
+    var drag = d3.behavior.drag().on("drag", dragmove);
+    var scale = d3.scale.linear()
+                  .domain([0, a])
+                  .range([0, 400]);
+    var svg = d3.select('.container').append('svg').attr('width', scale(room_length)).attr('height', scale(room_width));
+    $('#addFurniture').on('submit', function(e) {
+    e.preventDefault();
+    var name = $('input[name=name]').val();
+    var width = $('input[name=furnWidth]').val();
+    var length = $('input[name=furnLength]').val();
+    dataset.push({name: name, width: width, length: length, x:0, y: 0});
+    furniture(dataset);
+  });
+  function dragmove(d) {
+    d3.select(this).attr("y", function() {
+      if (d3.event.y < 0) {
+        return 0;
+      } else if (d3.event.y > (scale(room_width) - this.getBBox().height)) {
+        return scale(room_width) - this.getBBox().height;
+      } else {
+        return d3.event.y;
+      }
+    })
+    .attr("x", function() {
+      if (d3.event.x < 0) {
+        return 0;
+      } else if (d3.event.x > (scale(room_length) - this.getBBox().width)) {
+        return scale(room_length) - this.getBBox().width;
+      } else {
+        return d3.event.x;
+      }
+    });
+  }
+  var drag = d3.behavior.drag().on("drag", dragmove);
+  function furniture(dataset) {  
+    var r = svg.selectAll('rect').data(dataset).enter().append('rect');
+    r.attr('x', function(d) {
+      return scale(d.x);
+    })
+    .attr('y', function(d) {
+      return scale(d.y);
+    })
+    .attr('width', function(d) {
+      return scale(d.width);
+    })
+    .attr('height', function(d) {
+      return scale(d.length);
+    })
+    .attr('fill', 'blue')
+    .call(drag);
+  }
+  });
+});
